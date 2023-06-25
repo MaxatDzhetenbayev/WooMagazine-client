3a38ea71bf48a381bd17288f45fa2726c5efaea1
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-06-25 23:54:58 +0600

    implement adaptability for header

--- src/pages/main/ui/Page/MainPage.tsx
+++ src/pages/main/ui/Page/MainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import style from './MainPage.module.scss'
-import { Link } from 'react-router-dom'
 
 import { EffectCards, Pagination, Mousewheel, Keyboard } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -20,9 +19,11 @@ export const MainPage = () => {
       <div className={style.main}>
          <div className={style.welcome}>
             <div className={style.banner}>
-               <Htag tag='h1'>Новые поступления в этом сезоне</Htag>
+               <Htag tag='h1' style={{ marginBottom: '42px' }}>
+                  Новые поступления в этом сезоне
+               </Htag>
                <div className={style.banner_content}>
-                  <PTag size='md' style={{ textAlign: 'right' }}>
+                  <PTag size='md' style={{ textAlign: 'right', marginBottom: '55px' }}>
                      Утонченные сочетания и бархатные оттенки - вот то, что вы искали в этом сезоне. Время исследовать.
                   </PTag>
                   <Button mode='primary'>Открыть магазин</Button>
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 import style from './Header.module.scss'
 import logo from '../../assets/dress.png'
 import phone from '../../assets/phone.png'
 import cart from '../../assets/cart 1.svg'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { GrClose } from 'react-icons/gr'
 
 export const Header = () => {
    const naviateItems = [
-      { name: 'Главнаяя', path: '/' },
+      { name: 'Главная', path: '/' },
       { name: 'Магазин', path: '/store' },
       { name: 'О Бренде', path: '/about' },
       { name: 'Контакты', path: '/contacts' },
    ]
+
+   const [isOpen, setIsOpen] = useState(false)
+
+   const handleMenuChange = (change: boolean) => {
+      setIsOpen(change)
+   }
+
+   const itemAnimate = {
+      open: { y: 0, opacity: 1, transition: { type: 'spring', stiffness: 300, damping: 15 } },
+      closed: { y: -20, opacity: 0, transition: { duration: 0.2 } },
+   }
 
    return (
       <div className={style.header}>
@@ -23,7 +37,17 @@ export const Header = () => {
             <ul className={style.navigate_list}>
                {naviateItems.map((item, index) => (
                   <li key={index} className={style.navigate_item}>
-                     <NavLink to={item.path}>{item.name}</NavLink>
+                     <NavLink
+                        style={({ isActive }) => {
+                           return {
+                              color: isActive ? `#509498` : '',
+                              transition: 'ease-in 0.3s ',
+                           }
+                        }}
+                        to={item.path}
+                     >
+                        {item.name}
+                     </NavLink>
                   </li>
                ))}
             </ul>
@@ -36,6 +60,66 @@ export const Header = () => {
             <NavLink to={'/'}>
                <img src={cart} alt='cart' />
             </NavLink>
+         </div>
+         <div className={style.burger}>
+            <motion.div
+               whileTap={{ scale: 0.5 }}
+               className={style.burger_icon}
+               onClick={() => handleMenuChange(!isOpen)}
+            >
+               {isOpen ? <GrClose /> : <GiHamburgerMenu />}
+            </motion.div>
+
+            <motion.div
+               animate={isOpen ? 'open' : 'closed'}
+               variants={{
+                  open: {
+                     opacity: 1,
+                     y: 0,
+                     height: 160,
+                     transition: {
+                        type: 'spring',
+                        delayChildren: 0.3,
+                        staggerChildren: 0.05,
+                     },
+                  },
+
+                  closed: {
+                     opacity: 0,
+                     y: -30,
+                     height: 0,
+                     transition: {
+                        type: 'spring',
+                        delay: 0.1,
+                        staggerChildren: 0.05,
+                     },
+                  },
+               }}
+               className={style.burger_window}
+            >
+               <div className={style.burger_wrapper}>
+                  <motion.nav className={style.burger_navigate}>
+                     <motion.ul className={style.burger_list}>
+                        {naviateItems.map((item, index) => (
+                           <motion.li variants={itemAnimate} key={index} className={style.burger_item}>
+                              <NavLink
+                                 style={({ isActive }) => {
+                                    return {
+                                       color: isActive ? `#000` : '#fff',
+                                       transition: 'ease-in 0.3s ',
+                                    }
+                                 }}
+                                 to={item.path}
+                                 onClick={() => handleMenuChange(false)}
+                              >
+                                 {item.name}
+                              </NavLink>
+                           </motion.li>
+                        ))}
+                     </motion.ul>
+                  </motion.nav>
+               </div>
+            </motion.div>
          </div>
       </div>
    )
