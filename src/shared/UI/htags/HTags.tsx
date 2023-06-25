3fbe9dfb3c817b4468c7e490d010255138ab72d7
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-06-26 01:11:28 +0600

    fix HTag component v2

--- src/shared/UI/htags/HTags.tsx
+++ src/shared/UI/htags/HTags.tsx
@@ -1,38 +1,20 @@
 import React, { DetailedHTMLProps, HtmlHTMLAttributes, ReactNode } from 'react'
 
-import styles from './Htags.module.scss'
-
 export interface HtagProps extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> {
    tag: 'h1' | 'h2' | 'h3' | 'h4'
    children: ReactNode
 }
 
-export const Htags = ({ tag, children, ...props }: HtagProps): JSX.Element => {
+export const Htags = ({ tag, children, ...props }: HtagProps) => {
    switch (tag) {
       case 'h1':
-         return (
-            <h1 className={styles.h1} {...props}>
-               {children}
-            </h1>
-         )
+         return <h1 {...props}>{children}</h1>
       case 'h2':
-         return (
-            <h2 className={styles.h2} {...props}>
-               {children}
-            </h2>
-         )
+         return <h2 {...props}>{children}</h2>
       case 'h3':
-         return (
-            <h3 className={styles.h3} {...props}>
-               {children}
-            </h3>
-         )
+         return <h3 {...props}>{children}</h3>
       case 'h4':
-         return (
-            <h3 className={styles.h4} {...props}>
-               {children}
-            </h3>
-         )
+         return <h3 {...props}>{children}</h3>
       default:
          return <></>
    }
